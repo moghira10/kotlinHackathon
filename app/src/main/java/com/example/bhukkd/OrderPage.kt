46b1540a18638c96c7caf754e599528b5ea26034
amardeepk <amardeepkumar15091996@gmail.com>
2019-04-04 18:11:50 +0530

    ui fixes

--- app/src/main/java/com/example/bhukkd/OrderPage.kt
+++ app/src/main/java/com/example/bhukkd/OrderPage.kt
@@ -6,6 +6,8 @@ import android.content.Intent
 import android.support.v7.widget.CardView
 import android.view.LayoutInflater
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -16,15 +18,24 @@ class OrderPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
         setContentView(R.layout.activity_order_page)
-        var ll :LinearLayout= findViewById(R.id.mainLL)
 
-        data class FoodItem (val name: String, val qty: Int, var price: Int)
-        var foodItems :Array<FoodItem> = arrayOf(FoodItem("Pizza",20, 20), FoodItem("Cheesy Fries", 10, 10))
+        this.supportActionBar?.title = "Review Your Order"
+
+        var ll: LinearLayout = findViewById(R.id.mainLL)
+
+        data class FoodItem(val name: String, val qty: Int, var price: Int)
+
+        var foodItems: Array<FoodItem> = arrayOf(FoodItem("Pizza", 20, 20), FoodItem("Cheesy Fries", 10, 10))
 
         ll.removeAllViews()
-        var total=0;
+        var total = 0;
         for (item in foodItems) {
 
             var v: View = LayoutInflater.from(this).inflate(R.layout.item, ll, false)
@@ -33,14 +44,14 @@ class OrderPage : AppCompatActivity() {
             itemName.setText("${item.name}   (Qty: ${item.qty})");
 
             var itemPrice = v.findViewById<TextView>(R.id.itPrice);
-            itemPrice.setText("Rs."+item.qty*item.price);
+            itemPrice.setText("Rs." + item.qty * item.price);
 
-            total += item.qty*item.price;
+            total += item.qty * item.price;
 
             ll.addView(v);
         }
         var totalView: TextView = findViewById(R.id.total);
-        totalView.setText("Total : Rs."+total);
+        totalView.setText("Total : Rs." + total);
 
         val checkout: TextView = findViewById(R.id.checkout)
 
